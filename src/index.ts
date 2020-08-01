0fbb64646312d2c1db65cb1871f979161b4a5bdd
RV <reijo.vosu.work@gmail.com>
2020-08-01 14:54:03 +0300

    add nyc and cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './application';
 import log from './lib/log';
-const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8001;
+const serverPort = process.env.PORT || 8001;
 
 app.listen(serverPort, () => {
   return log.info(`server is listening on ${serverPort}`);
--- test/application.spec.ts
+++ test/application.spec.ts
@@ -7,13 +7,12 @@ chai.use(chaiHttp);
 
 describe('App', () => {
   it('works', (done: () => void): void => {
-  chai.request(app)
+    chai.request(app)
       .get('/docs/')
       .send({})
       .end((err: Error, res: any): void => {
-          expect(res.statusCode).to.be.equal(200);
-          done();
+        expect(res.statusCode).to.be.equal(200);
+        done();
       });
-
-    });
+  });
 });
